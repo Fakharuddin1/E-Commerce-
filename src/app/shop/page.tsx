21276aa3ffc812a6eba4eeb3b32b42cde55e102e
Fakharuddin1 <160948161+Fakharuddin1@users.noreply.github.com>
2025-02-06 03:46:51 +0500

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,6 +3,15 @@ import Feature from "@/components/Feature";
 import Products from "../query/Products/page";
 import Link from "next/link";
 
+const client = createClient({
+  projectId: "9f3ykjul",
+  dataset: "production",
+  apiVersion: '2025-01-18',
+  useCdn: true, // Use `false` to always fetch fresh data
+  token: process.env.SANITY_API_TOKEN, // Securely use the token
+});
+
+
 const ShopPage = () => {
 
 
